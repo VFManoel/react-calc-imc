3a69c2c5047df00dfcc3fb5300827f490b330bd2
Manoel Victor Ferreira <manoel.vict.fer@gmail.com>
2026-05-31 16:27:39 -0300

    creating project, creating inputs and creating bmi calculator

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import styles from "./App.module.css";
+import poweredImage from "./assets/powered.png";
+import { levels, calculateBmi } from "./helpers/BMI";
 
-function App() {
+const App = () => {
+  const [heightField, setHeightField] = useState<number>(0);
+  const [weightField, setWeightField] = useState<number>(0);
+
+  const handleCalculateButton = () => {
+    if (heightField && weightField) {
+    } else {
+      alert("Type the missing information");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={styles.main}>
+      <header>
+        <div className={styles.headerContainer}>
+          <img src={poweredImage} alt="" width={150} />
+        </div>
       </header>
+      <div className={styles.container}>
+        <div className={styles.leftSide}>
+          <h1>BMI Calculator</h1>
+          <p>
+            Body Mass Index is a screening tool that uses a person's height and
+            weight to estimate their body fat and screen for potential health
+            risks.
+          </p>
+
+          <input
+            type="number"
+            placeholder="Type your height (in meters)"
+            value={heightField > 0 ? heightField : ""}
+            onChange={(e) => setHeightField(parseFloat(e.target.value))}
+          />
+          <input
+            type="number"
+            placeholder="Type your weight (in kilograms)"
+            value={weightField > 0 ? weightField : ""}
+            onChange={(e) => setWeightField(parseFloat(e.target.value))}
+          />
+
+          <button onClick={handleCalculateButton}>CALCULATE</button>
+        </div>
+        <div className={styles.rightSide}>
+          
+        </div>
+      </div>
     </div>
   );
-}
-
+};
 export default App;
